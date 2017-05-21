48fe30533df2ca9844f6b76bc1de3a84d07b293e
marcAndreTremblay <marct549@gmail.com>
2017-05-21 16:18:10 -0400

    Ground work for XML parser and data reading and formating for sql insertion

--- LOG660_InsertionProgram/Program.cs
+++ LOG660_InsertionProgram/Program.cs
@@ -4,13 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+
+using System.Xml;
+using System.IO;
+using System.Text;
+
 namespace LOG660_InsertionProgram
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello world");
+            FileStream fs = File.Open("./clients_latin1.xml",FileMode.Open);
+            XmlReader r = XmlReader.Create(fs);
+            while (r.Read())
+            {
+                if (r.NodeType == XmlNodeType.Element)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("<" + r.Name + ">");
+                    if (r.HasAttributes)
+                    {
+                        for (int i = 0; i < r.AttributeCount; i++)
+                        {
+                            Console.WriteLine("\tATTRIBUTE: " + r.GetAttribute(i));
+                        }
+                    }
+                }
+                else if (r.NodeType == XmlNodeType.Text)
+                {
+                    Console.WriteLine("\tVALUE: " + r.Value);
+                }
+            }
+
+            System.Console.ReadKey();
+
         }
     }
 }
---
+++ LOG660_InsertionProgram/XMLRawData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml;
+using System.IO;
+using System.Text;
+
+namespace LOG660_InsertionProgram
+{
+    public class XMLFilmData
+    {
+        public XMLFilmData()
+        {
+
+        }
+    }
+    public class XMLClientData
+    {
+        string last_name;
+        string first_name;
+        string courriel;
+        string telephone;
+        string aniversaire;
+        string address;
+        string ville;
+        public XMLClientData()
+        {
+
+        }
+    }
+    public class XMLPersonneData
+    {
+        public XMLPersonneData()
+        {
+
+        }
+    }
+}

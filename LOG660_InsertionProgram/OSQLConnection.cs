65a14f26b44a2b9cb87a2156fa05270bbe97d5c5
marcAndreTremblay <marct549@gmail.com>
2017-05-22 11:30:24 -0400

    Connect the project with oracle data access , base communication with the database

---
+++ LOG660_InsertionProgram/OSQLConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Oracle.DataAccess.Client;
+
+namespace LOG660_InsertionProgram
+{
+    public class OSQLConnection
+    {
+        public OSQLConnection()
+        {
+
+        }
+    }
+}

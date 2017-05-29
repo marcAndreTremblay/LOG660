8d2c20d5b96bb6f26a1013b654d060255f3226f8
marcAndreTremblay <marct549@gmail.com>
2017-05-29 12:42:19 -0400

    Create did remise directory for teacher

--- log660-labo1-equipe4/READ_ME.txt
+++ log660-labo1-equipe4/READ_ME.txt
@@ -1,8 +1,10 @@
 Notre application est un projet visual studio v.2013
 
 Démarer l'application :
+	-Detruire la BD avec les scripte ./SQL/DestructionTable.sql	
+	-Lancer le scripte de construction dans le ./SQL/DatabaseBuild.sql	
 	-Lancer : ./Bin/LOG660_InsertionProgram.exe (L'éxécution prend environ 10min)
-
+	-Lancer le scripte ./SQL/ProcedureTrigger.sql		
 Code Source :
 	Le dossier ./LOG660_InsertionProgram contient le code source ouvrable avec visual studio
 
--- log660-labo1-equipe4/SQL/ProcedureTrigger.sql
+++ log660-labo1-equipe4/SQL/ProcedureTrigger.sql
@@ -1,160 +1,9 @@
-CREATE TABLE  PERSONNE(
-PersonneID INTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
-prenom VARCHAR(100),
-nomFamille VARCHAR(100),
-dateNaissance DATE,
- biographie VARCHAR(2000), 
- lieuNaissance VARCHAR(100),
-PRIMARY KEY(PersonneID));
-
-CREATE TABLE Realisateur(
- fk_FilmID INTEGER,
- fk_PersonneID INTEGER);
- 
-CREATE TABLE Scenariste(
- fk_FilmID INTEGER,
- nom VARCHAR(50));
- 
-
-
-CREATE TABLE Client
-(ClientID INTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1), 
-PersonneID INTEGER, 
-ForfaitID INTEGER, 
-AdresseID INTEGER,
-CarteCreditID INTEGER, 
-numeroTel VARCHAR(20),
-courriel VARCHAR(50) UNIQUE, 
-password VARCHAR(100),  
-PRIMARY KEY(ClientID),
-CHECK (REGEXP_LIKE(password, '^[a-zA-Z0-9]{5,}$')));
-
-CREATE TABLE Employe
-(EmployeID iNTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1), 
-PersonneID INTEGER, 
-AdresseID INTEGER, 
-numeroTel VARCHAR(20), 
-courriel VARCHAR(50) UNIQUE, 
-password VARCHAR(100), 
-matricule VARCHAR(7),  
-PRIMARY KEY(EmployeID),
-CHECK (REGEXP_LIKE(password, '^[a-zA-Z0-9]{5,}$')));
-
-CREATE TABLE CarteCredit
-(CarteCreditID INTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1), 
-typeCarte VARCHAR(10), 
-numero VARCHAR(20), 
-exp_month INTEGER, 
-exp_year INTEGER, 
-cvv INTEGER,  
-PRIMARY KEY(CarteCreditID));
-
-CREATE TABLE Forfait
-(ForfaitID INTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1), 
-coutParMois INTEGER, 
-typeForfait VARCHAR(15), 
-locationMax INTEGER, 
-dureeMaxJour INTEGER,  
-PRIMARY KEY(ForfaitID));
-
-CREATE TABLE Film_Acteur(
-fk_personneID INTEGER, 
-fk_filmID INTEGER, 
-personnage VARCHAR(100),
-PRIMARY KEY(fk_personneID, fk_filmID));
-
-CREATE TABLE Inventaire
-(CodeCopieID INTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1), 
-FilmID INTEGER,  
-PRIMARY KEY(CodeCopieID));
-
-CREATE TABLE Adresse
-(AdresseID INTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1), 
-noCivique VARCHAR(10), 
-rue VARCHAR(50), 
-ville VARCHAR(50), 
-province VARCHAR(50), 
-codePostal VARCHAR(10),  
-PRIMARY KEY(AdresseID));
-
-CREATE TABLE Location_Client
-(LocationID INTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1), 
-CodeCopieID INTEGER, 
-ClientID INTEGER, 
-dateLocation DATE, 
-dateRetour DATE,  
-PRIMARY KEY(LocationID));
-
-CREATE TABLE Film
-(FilmID INTEGER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
-annee INTEGER, 
-titre VARCHAR(100), 
-pays VARCHAR(50), 
-langueOriginale VARCHAR(50), 
-genres VARCHAR(1000), 
-resumeFilm VARCHAR(4000), 
-dureeMinutes INTEGER,
-PRIMARY KEY(FilmID));
-
-ALTER TABLE Client
-ADD CONSTRAINT FK_AdresseClient
-FOREIGN KEY (AdresseID) REFERENCES Adresse(AdresseID);
-
-ALTER TABLE Client
-ADD CONSTRAINT FK_ForfaitClient
-FOREIGN KEY (ForfaitID) REFERENCES Forfait(ForfaitID);
-
-ALTER TABLE Client
-ADD CONSTRAINT FK_PersonneClient
-FOREIGN KEY (PersonneID) REFERENCES Personne(PersonneID);
-
-ALTER TABLE Client
-ADD CONSTRAINT FK_CarteCreditClient
-FOREIGN KEY (CarteCreditID) REFERENCES CarteCredit(CarteCreditID);
-
-ALTER TABLE Film_Acteur
-ADD CONSTRAINT FK_Acteur_ActeurFilm
-FOREIGN KEY (fk_personneID) REFERENCES Personne(PersonneID);
-
-ALTER TABLE Film_Acteur
-ADD CONSTRAINT FK_Film_ActeurFilm
-FOREIGN KEY (fk_filmID) REFERENCES Film(FilmID);
-
-ALTER TABLE Realisateur
-ADD CONSTRAINT FK_RealisateurPersonne
-FOREIGN KEY (fk_PersonneID) REFERENCES Personne(PersonneID);
-
-ALTER TABLE Realisateur
-ADD CONSTRAINT FK_RealisateurFilm
-FOREIGN KEY (fk_FilmID) REFERENCES Film(FilmID);
-
-ALTER TABLE Scenariste
-ADD CONSTRAINT FK_ScenaristeFilm
-FOREIGN KEY (fk_FilmID) REFERENCES Film(FilmID);
-
-
-ALTER TABLE Inventaire
-ADD CONSTRAINT FK_FilmInventaire
-FOREIGN KEY (FilmID) REFERENCES Film(FilmID);
-
-ALTER TABLE Location_Client
-ADD CONSTRAINT FK_InventaireLocation_Client
-FOREIGN KEY (CodeCopieID) REFERENCES Inventaire(CodeCopieID);
-
-ALTER TABLE Location_Client
-ADD CONSTRAINT FK_ClientLocation_Client
-FOREIGN KEY (ClientID) REFERENCES Client(ClientID);
-
-
-
-
-
 CREATE OR REPLACE TRIGGER VerifierDateExpirationCarte
 BEFORE INSERT ON CarteCredit
 FOR EACH ROW
 BEGIN
 	IF :NEW.EXP_YEAR < EXTRACT(YEAR FROM SYSDATE) OR :NEW.EXP_YEAR = EXTRACT(YEAR FROM SYSDATE) AND :NEW.EXP_MONTH < EXTRACT(MONTH FROM SYSDATE) THEN
-		RAISE_APPLICATION_ERROR('-20000', 'Carte de crÃ©dit expirÃ©');
+		RAISE_APPLICATION_ERROR('-20000', 'Carte de crédit expiré');
 	END IF;
 END;
 
@@ -183,7 +32,7 @@ DECLARE
 BEGIN
 	SELECT COUNT(*) INTO EstLoue FROM Location_Client WHERE CodeCopieID = :NEW.CodeCopieID AND dateRetour IS NULL;
 	IF EstLoue <> 0 THEN
-		RAISE_APPLICATION_ERROR('-20000', 'La copie doit Ãªtre disponible pour pouvoir la louer');
+		RAISE_APPLICATION_ERROR('-20000', 'La copie doit être disponible pour pouvoir la louer');
 	END IF;
 END;
 
@@ -230,7 +79,7 @@ BEGIN
     VALUES (adresseID, carteCreditID, personneID, forfaitID_in, courriel_in, numeroTel_in, password_in);
 
 EXCEPTION 
-WHEN OTHERS THEN raise_application_error(-20001,'Erreur dans la procÃ©dure pCreerClient - '||SQLCODE||' -ERROR- '||SQLERRM);
+WHEN OTHERS THEN raise_application_error(-20001,'Erreur dans la procédure pCreerClient - '||SQLCODE||' -ERROR- '||SQLERRM);
 END pCreerClient;
 
 /

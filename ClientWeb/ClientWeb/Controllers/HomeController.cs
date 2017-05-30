1316835a72dd6e7b2b2889d09a21ffdd736e88ae
Manda <mandaniaina@gmail.com>
2017-05-30 10:44:23 -0400

    Page de connexion

--- ClientWeb/ClientWeb/Controllers/HomeController.cs
+++ ClientWeb/ClientWeb/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ClientWeb.Controllers
 {
@@ -20,10 +16,8 @@ namespace ClientWeb.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Connexion()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ ClientWeb/ClientWeb/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ClientWeb.Models
+{
+    public class User
+    {
+        public bool isConnected { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}

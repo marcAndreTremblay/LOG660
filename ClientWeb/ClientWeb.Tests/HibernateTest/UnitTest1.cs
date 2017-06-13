51a50a05553328ed9ab02d12d096082fbf76cbac
marcAndreTremblay <marct549@gmail.com>
2017-06-13 10:43:08 -0400

    Base test file for hibernate

---
+++ ClientWeb/ClientWeb.Tests/HibernateTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+using ClientWeb.Models;
+
+namespace ClientWeb.Tests.HibernateTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void HibernateConnectionTest()
+        {
+            ISession session = NHibernateSession.OpenSessionNoServer();
+            Assert.IsNotNull(session);
+        }
+    }
+}

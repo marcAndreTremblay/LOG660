730e1424fae7497e3d4bdc048671ce93f6b27234
Alexandre Godard <alexandre@godard.me>
2017-06-15 00:35:24 -0400

    add mapping for rents, copies

--- ClientWeb/ClientWeb/Models/LocationClient.cs
+++ ClientWeb/ClientWeb/Models/LocationClient.cs
@@ -4,10 +4,10 @@ namespace ClientWeb.Models
 {
     public class LocationClient
     {
-        public Client Client { get; set; }
-        public DateTime DateLocation { get; set; }
-        public DateTime DateRetour { get; set; }
-        public int Id { get; set; }
-        public Inventaire Inventaire { get; set; }
+        public virtual Client Client { get; set; }
+        public virtual DateTime DateLocation { get; set; }
+        public virtual DateTime DateRetour { get; set; }
+        public virtual int Id { get; set; }
+        public virtual Inventaire Inventaire { get; set; }
     }
 }

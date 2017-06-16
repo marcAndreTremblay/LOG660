41effa84141c5940b93a750081f3288edbd084c6
marcAndreTremblay <marct549@gmail.com>
2017-06-16 11:48:32 -0400

    Test now is owrking dont delete it

--- ClientWeb/ClientWeb.Tests/HibernateTest.cs
+++ ClientWeb/ClientWeb.Tests/HibernateTest.cs
@@ -16,11 +16,26 @@ namespace ClientWeb.Tests
     [TestFixture]
     public class HibernateTest
       {
+        ISession sut_session;
+        [SetUp]
+        public void Init()
+        {
+            sut_session = NHibernateSession.OpenSessionNoServer();
+        }
+
         [Test]
         public void HibernateSessionTest()
-         {
-             ISession session = NHibernateSession.OpenSessionNoServer();
-             Assert.IsNotNull(session);
-         }
+        {
+             Assert.IsNotNull(sut_session);
+        }
+
+
+
+        [Test]
+        public void Hibernate_Get_All_Client()
+        {
+            IList<Client> results = sut_session.QueryOver<Client>().List<Client>();        
+            Assert.IsNotNull(results);
+        }
     }
 }

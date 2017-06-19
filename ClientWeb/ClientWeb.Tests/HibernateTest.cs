7524c04b2082b7806ff31d64310663ff5b8f92f4
marcAndreTremblay <marct549@gmail.com>
2017-06-19 15:37:13 -0400

    add dao test

--- ClientWeb/ClientWeb.Tests/HibernateTest.cs
+++ ClientWeb/ClientWeb.Tests/HibernateTest.cs
@@ -55,15 +55,16 @@ namespace ClientWeb.Tests
 
 
 
-        [TestCase("root", "root", true, TestName = "right password , rignt matricule")]
-        [TestCase("sadads", "root", false , TestName = "Wrong password , rignt matricule")]
-        [TestCase("dsdadsd", "dsadads",false, TestName = "Wrong password , wrong matricule")]
-        [TestCase("root", "dsadada",false, TestName = "right password , wrong matricule")]
+        [TestCase("root", "root", true, TestName = "Employe search : Right password , Rignt matricule")]
+        [TestCase("sadads", "root", false , TestName = "Employe search : Wrong password , Rignt matricule")]
+        [TestCase("dsdadsd", "dsadads",false, TestName = "Employe search : Wrong password , Wrong matricule")]
+        [TestCase("root", "dsadada",false, TestName = "Employe search : Right password , Wrong matricule")]
         public void Employe_Connection(string mdp, string matricule,bool expected_value)
         {
-            Employe sut = Employe.TrouverEmployeParMatriculeEtMotDePasse(matricule, mdp);
+            EmployeDao sut = new EmployeDao();
+            Employe result_employe = sut.GetEmployeParMatriculeEtMotDePasse(matricule, mdp);
             bool result;
-            if(sut == null) {
+            if(result_employe == null) {
                 result = false;
             }
             else {
@@ -71,13 +72,13 @@ namespace ClientWeb.Tests
             }
             Assert.AreEqual(result, expected_value);
         }
-        [TestCase("hello123", "AnneDBrown75@hotmail.com", true, TestName = "right password , rignt username")]
-        [TestCase("sadads", "AnneDBrown75@hotmail.com", false, TestName = "Wrong password , rignt username")]
-        [TestCase("hello123", "dsadads", false, TestName = "Wrong password , wrong username")]
-        [TestCase("root", "dsadada", false, TestName = "right password , wrong username")]
+        [TestCase("hello123", "AnneDBrown75@hotmail.com", true, TestName = "Client connection : Right password , Rignt username")]
+        [TestCase("sadads", "AnneDBrown75@hotmail.com", false, TestName =  "Client connection : Wrong password , Rignt username")]
+        [TestCase("hello123", "dsadads", false, TestName =  "Client connection : Wrong password , Wrong username")]
+        [TestCase("root", "dsadada", false, TestName = "Client connection : Right password , Wrong username")]
         public void Client_Connection(string mdp, string email, bool expected_value)
         {
-            ClientDao sut =new  ClientDao();
+            ClientDao sut = new  ClientDao();
             Client result_client = sut.GetClientParCourrielEtMotDePasse(email, mdp);
             bool result;
             if (result_client == null)
@@ -90,6 +91,44 @@ namespace ClientWeb.Tests
             }
             Assert.AreEqual(result, expected_value);
         }
+
+
+
+        [TestCase(1, true, TestName = "Film seach : Valid ID")]
+        [TestCase(40000, false, TestName = "Film seach: Large invalid ID")]
+        [TestCase(-100, false, TestName = "Film seach : Negative invalid ID")]
+        public void Film_Seach_test(int target_id, bool expected_value)
+        {
+            FilmDao sut = new FilmDao();
+            Film result_data = sut.GetFilmParId(target_id);
+            bool result;
+            if (result_data == null)
+            {
+                result = false;
+            }
+            else
+            {
+                result = true;
+            }
+            Assert.AreEqual(result, expected_value);
+        }
+
+        [Test]
+        public void Film_Location_Transaction()
+        {
+            FilmDao film_dao = new FilmDao();
+            ClientDao client_dao = new ClientDao();
+            Client result_client = client_dao.GetClientParCourrielEtMotDePasse("AnneDBrown75@hotmail.com", "hello123");
+
+
+            Film f1 = film_dao.GetFilmParId(1);
+            int check1 = film_dao.GetNbCopiesRestantes(f1.Id);
+            film_dao.LouerCopie(f1.Id, result_client.Id);
+            int check2 = film_dao.GetNbCopiesRestantes(f1.Id);
+
+            Assert.AreEqual(check1 - 1, check2);
+        }
     }
     
+
 }

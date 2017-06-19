1cbdec8d84d03b1cc5dd6eff516df3d8d126c9b2
Alexandre Godard <alexandre@godard.me>
2017-06-19 14:46:46 -0400

    implement DAO pattern

--- ClientWeb/ClientWeb/Models/Client.cs
+++ ClientWeb/ClientWeb/Models/Client.cs
@@ -16,54 +16,6 @@ namespace ClientWeb.Models
         public virtual string NumeroTel { get; set; }
         public virtual string Password { get; set; }
         public virtual Personne Personne { get; set; }
-
-        public static Client TrouverClientParCourrielEtMotDePasse(string email, string mdp)// a faire
-        {
-            using (ISession session = NHibernateSession.OpenSession())
-            {
-                Client UnClient = null;
-                using (var tx = session.BeginTransaction())
-                {
-                    try
-                    {
-                        UnClient = session.CreateCriteria<Client>()
-                           .Add(Restrictions.Eq("Courriel", email))
-                           .Add(Restrictions.Eq("Password", mdp))
-                           .UniqueResult<Client>();
-
-                        tx.Commit();
-                    }
-                    catch (NonUniqueResultException)
-                    {
-                        return null;
-                    }
-                }
-
-                return UnClient;
-            }
-        }
-
-        public virtual int GetNbLocationsEnCours()
-        {
-            using (ISession session = NHibernateSession.OpenSession())
-            {
-                int nbCopiesLouees = 0;
-                using (var tx = session.BeginTransaction())
-                {
-                    nbCopiesLouees = (int) session.CreateCriteria<LocationClient>()
-                        .Add(Restrictions.Eq("Client.Id", Id))
-                        .Add(Restrictions.Or(
-                            Restrictions.Eq("DateRetour", Convert.ToDateTime("0001-01-01")),
-                            Restrictions.IsNull("DateRetour")
-                        ))
-                        .SetProjection(Projections.CountDistinct("Id"))
-                        .UniqueResult();
-
-                    tx.Commit();
-                }
-
-                return nbCopiesLouees;
-            }
-        }
+        public int NbLocationsEnCours { get; set; }
     }
 }

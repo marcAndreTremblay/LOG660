a384eccf3c2bef68b1ad8e6698f94a09b5ea6e1a
marcAndreTremblay <marct549@gmail.com>
2017-07-20 11:17:05 -0400

    Scray queryyaa

--- SQL/Link_And_View.sql
+++ SQL/Link_And_View.sql
@@ -1,8 +1,3 @@
-CREATE DATABASE LINK  test_link
-CONNECT TO EXT660E IDENTIFIED BY "LoG660x" USING 'EXT660';
-
-CREATE SYNONYM Proxy_Data_Table
-FOR LOG6601C.COTES@test_link;
 
 -- Get specific cote for 'x' movie and 'y' client
     --select COTE from Proxy_Data_Table where IDFILM = 47478 and IDCLIENT = 913554;
@@ -10,7 +5,109 @@ FOR LOG6601C.COTES@test_link;
 -- Get specific movie average cote value
  -- select (sum(COTE)/count(IDFILM)) from Proxy_Data_Table where IDFILM = 47478;    
 -- Retrive all client that already coted both movie  
- -- select count(IDCLIENT) from Proxy_Data_Table where IDFILM = 47478 or IDFILM = 66011;
+
+-- Upper therme
+    select   
+        sum((tt1.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = 47478) )*
+         (tt2.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = 66011)))
+     from 
+        (select * from Proxy_Data_Table where IDFILM = 47478) tt1,
+        (select * from Proxy_Data_Table where IDFILM = 66011) tt2
+    where tt1.IDCLIENT = tt2.IDClIENT;
+
+
+
+select FILMID,
+    -- upper therme
+     (select   
+        sum((tt1.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = f_j.FILMID) )*
+         (tt2.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = 66011)))
+     from 
+        (select * from Proxy_Data_Table where IDFILM = f_j.FILMID) tt1,
+        (select * from Proxy_Data_Table where IDFILM = 66011) tt2
+    where tt1.IDCLIENT = tt2.IDClIENT) as value_s1,
+    -- first lower therme
+    (select   
+    (sum(Power((cote - (select sum(cote)/count(IDCLIENT) 
+        from  Proxy_Data_Table 
+     where IDFILM = f_j.FILMID)),2))) from 
+    (select * from Proxy_Data_Table where IDFILM = f_j.FILMID ) ) as value_s2,
+    -- secound lower therme
+    (select   
+    (sum(Power((cote - (select sum(cote)/count(IDCLIENT) 
+        from  Proxy_Data_Table 
+     where IDFILM = 66011)),2)))  from 
+    (select * from Proxy_Data_Table where IDFILM = 66011 )) as value_s3
+from FILM f_j;
+
+
+
+
+select FILMID,
+    -- upper therme
+     (select   
+        sum((tt1.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = f_j.FILMID) )*
+         (tt2.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = 66011)))
+     from 
+        (select * from Proxy_Data_Table where IDFILM = f_j.FILMID) tt1,
+        (select * from Proxy_Data_Table where IDFILM = 66011) tt2
+    where tt1.IDCLIENT = tt2.IDClIENT) / SQRT((
+    -- first lower therme
+    (select   
+    (sum(Power((cote - (select sum(cote)/count(IDCLIENT) 
+        from  Proxy_Data_Table 
+     where IDFILM = f_j.FILMID)),2))) from 
+    (select * from Proxy_Data_Table where IDFILM = f_j.FILMID ) )) *(
+    -- secound lower therme
+    (select   
+    (sum(Power((cote - (select sum(cote)/count(IDCLIENT) 
+        from  Proxy_Data_Table 
+     where IDFILM = 66011)),2)))  from 
+    (select * from Proxy_Data_Table where IDFILM = 66011 )) )) as Correlation_value
+from FILM f_j;
+
+
+
+-- This is the super querry that will calcule the correlation of every movie again all movie
+select f_j.FILMID,f_k.FILMID,
+    -- upper therme
+     (select   
+        sum((tt1.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = f_j.FILMID) )*
+         (tt2.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = f_k.FILMID)))
+     from 
+        (select * from Proxy_Data_Table where IDFILM = f_j.FILMID) tt1,
+        (select * from Proxy_Data_Table where IDFILM = f_k.FILMID) tt2
+    where tt1.IDCLIENT = tt2.IDClIENT) / SQRT((
+    -- first lower therme
+    (select   
+    (sum(Power((cote - (select sum(cote)/count(IDCLIENT) 
+        from  Proxy_Data_Table 
+     where IDFILM = f_j.FILMID)),2))) from 
+    (select * from Proxy_Data_Table where IDFILM = f_j.FILMID ) )) *(
+    -- secound lower therme
+    (select   
+    (sum(Power((cote - (select sum(cote)/count(IDCLIENT) 
+        from  Proxy_Data_Table 
+     where IDFILM = f_k.FILMID)),2)))  from 
+    (select * from Proxy_Data_Table where IDFILM = f_k.FILMID )) )) as Correlation_value
+from (select FILMID from FILM) f_k ,FILM f_j;
+
 
 
 drop MATERIALIZED VIEW ma_vue_moyenne;
@@ -18,3 +115,36 @@ create MATERIALIZED VIEW ma_vue_moyenne
 REFRESH FORCE START WITH SYSDATE
 NEXT TRUNC(SYSDATE) + 8 
 AS select IDFILM as Id_Movie,(sum(COTE)/count(IDFILM)) as Average from Proxy_Data_Table group by IDFILM;
+
+
+drop MATERIALIZED VIEW ma_vue_correlation;
+--create MATERIALIZED VIEW ma_vue_correlation
+--REFRESH FORCE START WITH SYSDATE
+--NEXT TRUNC(SYSDATE) + 15 
+create  VIEW ma_vue_correlation_not
+AS select FILMID,
+    -- upper therme
+     (select   
+        sum((tt1.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = f_j.FILMID) )*
+         (tt2.cote - (select sum(cote)/count(IDCLIENT) 
+            from  Proxy_Data_Table 
+         where IDFILM = 66011)))
+     from 
+        (select * from Proxy_Data_Table where IDFILM = f_j.FILMID) tt1,
+        (select * from Proxy_Data_Table where IDFILM = 66011) tt2
+    where tt1.IDCLIENT = tt2.IDClIENT) / SQRT((
+    -- first lower therme
+    (select   
+    (sum(Power((cote - (select sum(cote)/count(IDCLIENT) 
+        from  Proxy_Data_Table 
+     where IDFILM = f_j.FILMID)),2))) from 
+    (select * from Proxy_Data_Table where IDFILM = f_j.FILMID ) )) *(
+    -- secound lower therme
+    (select   
+    (sum(Power((cote - (select sum(cote)/count(IDCLIENT) 
+        from  Proxy_Data_Table 
+     where IDFILM = 66011)),2)))  from 
+    (select * from Proxy_Data_Table where IDFILM = 66011 )) )) as Correlation_value
+from FILM f_j;
